efba21c28c546a3e6775ca5716079ca51b88165d
javecs <moonhee.javecs@gmail.com>
2017-04-25 00:10:34 +0900

    App Engineで動かせる環境設定をしました。

--- src/main/kotlin/xyz/javecs/CalcApplication.kt
+++ src/main/kotlin/xyz/javecs/CalcApplication.kt
@@ -2,9 +2,17 @@ package xyz.javecs
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class CalcApplication
+
+@RestController
+class HealthCheck {
+    @RequestMapping("/_ah/health")
+    fun healthy() = "Still surviving."
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(CalcApplication::class.java, *args)
